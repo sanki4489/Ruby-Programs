a625b48049b985e9814dc878276509b2345888d9
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-03-13 12:15:38 +0530

    Add files via upload

---
+++ roman_numerals.rb
@@ -0,0 +1,26 @@
+#Used IRB to run this program
+
+
+number_map = [nil, 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+# => [nil, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
+number_map = {
+  1 => 'I',
+  2 => 'II',
+  3 => 'III',
+  4 => 'IV',
+  5 => 'V',
+  6 => 'VI',
+  7 => 'VII',
+  8 => 'VIII',
+  9 => 'IX',
+  10 => 'X'
+}
+# => {1=>"I", 2=>"II", 3=>"III", 4=>"IV", 5=>"V", 6=>"VI", 7=>"VII", 8=>"VIII", 9=>"IX", 10=>"X"}
+
+#Examples of working
+puts number_map[4]
+# 'IV'
+# => nil
+number_map[9] == 'IX'
+# => true
+

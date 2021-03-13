f7c1712fbcf9a36ba1c330c933dac1410e98828b
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-03-13 15:43:57 +0530

    Add files via upload

---
+++ blanket_patterns_solution.rb
@@ -0,0 +1,10 @@
+# Usig interators to solve the problem
+colors = "RRGGBBYYKK"
+lines = 20
+
+lines.times do |i|
+  first = colors[0]
+  rest = colors[1..-1]
+  colors = rest + first
+  puts colors
+end
